3a331fabdbe46a7a62685163997f6012df4de2e4
Gungor Basa <gungorbasa@gmail.com>
2021-06-13 13:00:13 +0300

    Post and Comment models are added

---
+++ D4L/Models/Comment.swift
@@ -0,0 +1,15 @@
+//
+//  Comment.swift
+//  D4L
+//
+//  Created by Gungor Basa on 6/13/21.
+//
+
+import Foundation
+
+struct Comment: Codable {
+  let id: Int
+  let name: String
+  let email: String
+  let body: String
+}
---
+++ D4L/Models/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  D4L
+//
+//  Created by Gungor Basa on 6/13/21.
+//
+
+import Foundation
+
+struct Post: Codable {
+  let id: Int
+  let title: String
+  let body: String
+}
